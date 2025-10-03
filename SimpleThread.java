d9be708f838eca2843ed3e257241037da6c40455
aj <aj.baker@cyburity.com>
2025-10-03 13:13:51 -0500

    initial commit

---
+++ SimpleThread.java
@@ -0,0 +1,17 @@
+public class SimpleThread{
+    public static void main(String[] args) {
+        MessageWriter msgThread = new MessageWriter("One");
+        Thread threadOne = new Thread(msgThread);
+        threadOne.start();
+
+        try {
+            threadOne.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\nmain program execution at end");
+        // System.exit(0);
+
+    }
+}
